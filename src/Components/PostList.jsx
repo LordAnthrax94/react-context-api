1d7061ff34ba82ac75755f972e0720b6da01b4c0
LordAnthrax94 <andreagi1994@gmail.com>
2025-01-27 17:26:59 +0100

    fix: impostato PostPage

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,32 +1,33 @@
-import axios from "axios"
-import { useState } from "react"
+import { useEffect } from "react"
+import { useGlobalContext } from "../context/PostContext";
 
 const baseApiUrl = import.meta.env.VITE_BASE_API_URL
 
 const postList = () =>{
-  
-  const [posts, setPosts] = useState(null)
-
-    const fetchData = () => {
-      axios.get(`${baseApiUrl}/posts/${id}`)
-        .then(res => {
-          
-          
-          setPosts(res.data)
-        })
-        .catch(err => {
-          console.log("Post non trovato:", err);        
-        })
-      }
+    const {posts, fetchPost} = useGlobalContext() 
 
       useEffect(() => {
         fetchData()
       }, [])
   
   return (
-    <>
-    </>
-    
+    <div className="container">
+      <div className="col-12">
+        <div className="card">
+          <h1>{post?.name}</h1>
+          <img src={post?.image} className="card-img-top img-fluid" alt={post?.name} />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="d-flex justify-content-between">
+            <button className="btn btn-warning" onClick={() => navigate(-1)}>Indietro</button>
+            <button className="btn btn-danger" onClick={handleDeletePost}>Elimina Post</button>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/Components/PostPage.jsx
@@ -0,0 +1,13 @@
+import PostList from "./PostList"
+
+
+const PostPage = () => {
+  return (
+    <div className="container">
+      <h3 className="text-center">Lista dei Post</h3>
+      <PostList />
+    </div>
+  );
+};
+
+export default PostPage;
